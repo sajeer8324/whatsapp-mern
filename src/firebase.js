36e4e9d0a348043b71b3ed865c8b49dd1c3c1b98
sajeer8324 <you@example.com>
2021-11-04 19:03:47 +0530

    chat-footer css prblm

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTx0bNWB0WRqZF1b6K7ulZ7dgNclSUGF4",
+    authDomain: "whatsapp-mern-53acc.firebaseapp.com",
+    projectId: "whatsapp-mern-53acc",
+    storageBucket: "whatsapp-mern-53acc.appspot.com",
+    messagingSenderId: "819694372413",
+    appId: "1:819694372413:web:a83f6d2066ffc02c9c4c54",
+  };
